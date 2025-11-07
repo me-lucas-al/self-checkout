7b49e685bacc739a74bff0959672908285e2221e
me-lucas-al <me.lucasalmeida@gmail.com>
2025-11-07 14:40:40 -0300

    fix: configure prisma seed and fill up database

--- prisma.config.ts
+++ prisma.config.ts
@@ -1,9 +1,11 @@
+import 'dotenv/config'
 import { defineConfig, env } from "prisma/config";
 
 export default defineConfig({
   schema: "prisma/schema.prisma",
   migrations: {
-    path: "prisma/migrations",
+    path: "./prisma/migrations",
+    seed: `pnpx tsx ./prisma/seed.ts`,
   },
   engine: "classic",
   datasource: {
