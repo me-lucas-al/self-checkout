904afe2b349a1a3e6fce06b34e508e9b345b0470
me-lucas-al <me.lucasalmeida.com>
2025-12-06 16:23:40 -0300

    add initial card product component

--- src/app/[slug]/menu/contexts/cart.tsx
+++ src/app/[slug]/menu/contexts/cart.tsx
@@ -13,6 +13,7 @@ export interface ICartContext {
   products: CartProduct[];
   toggleCart: () => void;
   addProduct: (product: CartProduct) => void;
+  decreaseProductQuantity: (productId: string) => void;
 }
 
 export const CartContext = createContext<ICartContext>({
@@ -20,6 +21,7 @@ export const CartContext = createContext<ICartContext>({
   products: [],
   toggleCart: () => {},
   addProduct: () => {},
+  decreaseProductQuantity: () => {},
 });
 
 export const CartProvider = ({ children }: { children: React.ReactNode }) => {
@@ -45,6 +47,18 @@ export const CartProvider = ({ children }: { children: React.ReactNode }) => {
         return prevProduct;
       });
     });
+
+    const decreaseProductQuantity = (productId: string) => {
+      setProducts((prevProducts) =>
+        prevProducts.map((prevProduct) => {
+          if (prevProduct.id !== productId || prevProduct.quantity === 1) {
+            return prevProduct;
+          }
+          return { ...prevProduct, quantity: prevProduct.quantity - 1 };
+        })
+      );
+    };
+
     return (
       <CartContext.Provider
         value={{
@@ -52,6 +66,7 @@ export const CartProvider = ({ children }: { children: React.ReactNode }) => {
           products,
           toggleCart,
           addProduct,
+          decreaseProductQuantity,
         }}
       >
         {children}
