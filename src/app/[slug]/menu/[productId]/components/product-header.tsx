7a974355fc290c6c85c5b51ffc076f03b3b11df4
me-lucas-al <me.lucasalmeida@gmail.com>
2026-02-19 01:09:36 -0300

    feat: remove scroll text icon from products header

--- src/app/[slug]/menu/[productId]/components/product-header.tsx
+++ src/app/[slug]/menu/[productId]/components/product-header.tsx
@@ -32,15 +32,6 @@ export default function ProductHeader({ product }: ProductHeaderProps) {
         fill
         className="object-contain"
       />
-
-      <Button
-        variant="secondary"
-        size="icon"
-        className="absolute top-4 right-4 z-50 rounded-full hover:cursor-pointer"
-        onClick={ ()=> router.push('/cart-sheet') }
-      >
-        <ScrollTextIcon />
-      </Button>
     </div>
   );
 }
